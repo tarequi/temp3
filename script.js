dc01115a8739ffb8df0df2c4b0b99ffaa93acae6
tareqanshasi <t.anshasi99@gmail.com>
2022-06-25 18:12:27 +0300

    uplode project

---
+++ script.js
@@ -0,0 +1,6 @@
+/*const iicon = document.getElementsByClassName('toggle-menu')[0]
+const navlinks = document.getElementsByClassName('links')[0]
+
+iicon.addEventListener('click', () =>{
+    navlinks.classList.toggle('actives')
+})*/
